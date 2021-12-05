921229bea3b8adedae0fa33bbc462ff7683978cd
Lucas Alonso <lucasalonso598@gmail.com>
2021-12-05 15:07:14 -0300

    Created name-getter.py script that gets 10 names from a website

---
+++ name-getter.py
@@ -0,0 +1,27 @@
+# This script will gather 10 names from a website and add them to a text file.
+
+import requests
+
+response = requests.get('https://ledgernote.com/blog/interesting/best-flute-players-in-the-world/')
+
+with open('Flute_html.txt', 'w') as file:
+    file.write(response.text)
+#The HTML code of the website is now stored on the Flute_html.txt file.
+
+names = open('Names.txt', 'w') #Create Names.txt file, where the 10 names will be stored.
+
+def create_namefile(html_file, names_file):
+    names = open(names_file, 'w')
+    with open(html_file) as file:
+        for linea in file.readlines():
+            if "<h2>#" in linea:
+                nombre = linea.split('</h2>')
+                nombres = nombre[0][9:].strip()
+                print(nombres)
+                names.write(nombres + '\n')
+                        
+create_namefile('Flute_html.txt', 'Names.txt')
+#Goes through each line of the Flute_html.txt file and gathers only the 10 names on the
+#titles with string manipulation, adding them to the Names.txt file.             
+            
+            
